82b59ddc67a94aca7a4d94da83664bf9383f6e9c
maschinlia <juliaweibert2004@gmail.com>
2025-11-22 13:00:38 +0100

    für beliebig viele Zahlen

---
+++ C-Programme/Übung3-31.c
@@ -0,0 +1,30 @@
+
+#include <math.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()  {
+    int N;
+    printf("Bitte geben Sie ein bis zu welcher Zahl die Primzahlen bestimmt werden sollen...\n");
+    scanf("%d", &N);
+    int primes[N];
+    int i, j;
+    primes[0] = 0;
+    primes[1] = 1;
+
+    for (i = 1; i < N; i++)
+        primes[i] = 1;
+
+    for (i = 2; i < N / 2; i++)
+    {
+        for (j = 2 * i; j < N; j += i)
+        {
+            primes[j] = 0;
+        }
+    }
+    for (i = 1; i < N; i++)
+        if (primes[i] == 1)
+            printf("%8d\n", i);
+
+    return 0;
+}
