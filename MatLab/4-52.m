82d3fcd0f7773060d58523f669efc8006a87d32f
klajon <klajon11@gmail.com>
2025-12-04 22:37:09 +0100

    4.52 verbesserung

--- MatLab/4-52.m
+++ MatLab/4-52.m
@@ -1,10 +1,27 @@
-n = 10;
-X = [0:4*pi/(n):4*pi]
-Y = sin(X)
-Y2 = circshift(Y,-1) - Y;
+n = 100;
+X = [0:4*pi/(n):4*pi];
+Y = sin(X);
 
-v = (circshift(Y,-1) - Y) ./ (circshift(X,-1) - X)
+%Teil 5
+v = (circshift(Y,-1) - Y) ./ (circshift(X,-1) - X);
+z = (circshift(Y,-1) - circshift(Y,+1)) ./ ((circshift(X,-1) - circshift(X,+1)));
+v(1,n+1) = NaN;
+z(1,1) = NaN;
+z(1,n+1) = NaN;
+%plot(X,v)
+%plot(X,z)
 
-z = (circshift(Y,-1) - circshift(Y,+1)) ./ ((circshift(X,-1) - circshift(X,+1)))
+%Teil 1-4 (check nicht warum man das so machen sollte wenn mans einfach wie
+%5 machen kann)
+Plus = circshift(eye(n+1),1);                       % Matrix die aus Y_k Y_k+1 macht
+Minus = circshift(eye(n+1),1,2);                    % Matrix die aus Y_k Y_k-1 macht
+Delta = circshift(eye(n+1),1) + (-1 .* eye(n+1));   % Matrix die Delta = Y_k+1 - Y macht
 
-plot(X,z)
+dV = (Y * Delta) ./ (X * Delta);
+dV(1,n+1) = NaN;
+
+Zelta = (Y * Plus - Y * Minus) ./ (X * Plus - X * Minus);
+Zelta(1,1) = NaN;
+Zelta(1,n+1) = NaN;
+%plot(X,dV)
+%plot(X,Zelta)
