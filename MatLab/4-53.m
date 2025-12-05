d8c0945fef80520b44cd9cfd432811fe3191cdfd
klajon <klajon11@gmail.com>
2025-12-05 13:16:41 +0100

    4-53 Teil 2

--- MatLab/4-53.m
+++ MatLab/4-53.m
@@ -1,28 +1,38 @@
 clear all
-n = 1000;
-t0 = 0; t1 = 100;
-y10 = 10000; lambda1 = -0.1;
-lambda2 = -0.03;
+n    = 1000;
+t0   = 0; t1 = 100;
+y10  = 10000;
+lambda1 = -0.1;   
+lambda2 = -0.03; 
 
+dt = (t1-t0)/n;
 
-dt = ( t1 - t0 ) / n ;
+T  = zeros(n+1,1);
+Y1 = zeros(n+1,1);
+Y2 = zeros(n+1,1);
 
-T = zeros ( n +1 ,1) ;
-Y1 = zeros ( n +1 ,1) ;
+T(1)  = t0;
+Y1(1) = y10;
+Y2(1) = 0;  
 
-T (1) = t0 ;
-Y1 (1) = y10 ;
-
-for i = 2: n +1
-T ( i ) = T (i -1) + dt ;
-Y1 ( i ) = Y1 (i -1) + dt * lambda1 * Y1 (i -1) ;
+for i = 2:n+1
+    T(i) = T(i-1) + dt;
+    Y1(i) = Y1(i-1) + dt * (lambda1 * Y1(i - 1));
+    Y2(i) = Y2(i-1) + dt * (lambda2 * Y2(i - 1) - lambda1 * Y1(i - 1));
 end
 
-P1 = plot (T , y10 * exp ( lambda1 * T ) ) ; %Anzahl Mutternuklide A
+Y1_exact = y10 * exp(lambda1 * T);
+
+Y2_exact = y10 * (-lambda1)/(lambda2-lambda1) * (exp(lambda2*T) - exp(lambda1*T));
+
+figure;
+p1 = plot(T, Y1_exact, 'LineWidth',4); 
 hold on
-P2 = plot (T , Y1 , '-o') ;
-hold on
-P3 = plot (T, (y10 - Y1) .* exp (lambda2 * T)); %Anzahl Tochternuklide B 
-hold off
-set ( P1 , 'LineWidth' ,4 , 'Color' ,[0 1 1]) ;
-set ( P2 , 'LineWidth' ,1 , 'Color' ,[1 0 0]) ;
+p2 = plot(T, Y1, 'o-', 'LineWidth',1); 
+p3 = plot(T, Y2_exact, 'LineWidth',4);
+p4 = plot(T, Y2, 'o-', 'LineWidth',1);
+legend('y_1 exact','y_1 Euler','y_2 exact','y_2 Euler','Location','northeast');
+xlabel('t'); ylabel('Anzahl Kerne');
+grid on;
+hold off;
+
