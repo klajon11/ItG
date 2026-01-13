e5de67bbfde11672d9c52260edfe31ea49f02715
klajon <klajon11@gmail.com>
2026-01-13 23:01:18 +0100

    Stand Bilder

--- Projekt/Aliasing.m
+++ Projekt/Aliasing.m
@@ -9,12 +9,10 @@ x = linspace(-2*pi, 2*pi, N);
 y = linspace(-2*pi, 2*pi, N);
 [X, Y] = meshgrid(x, y);
 
-% Räumliche Frequenz
-f = 10;   % je größer, desto mehr Aliasing später
 
 % Abstandscosinus
-R = sqrt(X.^2 + Y.^2);
-original = cos(f * R);
+RR = X.^2 + Y.^2;
+original = cos(1.1 * RR);
 
 % Plot
 %figure
@@ -27,10 +25,10 @@ original = cos(f * R);
 %% ----------
 % Sampling des Originalbildes auf 19x19
 
-x19 = linspace(-2*pi, 2*pi, 19);
-y19 = linspace(-2*pi, 2*pi, 19);
-[X19, Y19] = meshgrid(x19, y19);
-sampled19 = interp2(X, Y, original, X19, Y19, 'nearest');
+x38 = linspace(-2*pi, 2*pi, 38);
+y38 = linspace(-2*pi, 2*pi, 38);
+[X38, Y38] = meshgrid(x38, y38);
+sampled38 = interp2(X, Y, original, X38, Y38, 'nearest');
 
 
 %% ---------
@@ -43,7 +41,7 @@ sampled13 = interp2(X, Y, original, X13, Y13, 'nearest');
 
 %% ---------
 %Interpolation auf 500x500
-interp19 = interp2(X19, Y19, sampled19, X, Y, 'linear');
+interp38 = interp2(X38, Y38, sampled38, X, Y, 'linear');
 interp13 = interp2(X13, Y13, sampled13, X, Y, 'linear');
 
 
@@ -53,13 +51,13 @@ interp13 = interp2(X13, Y13, sampled13, X, Y, 'linear');
 %Bedeutet dass man bevor man sampelt den ganzen spaß mittelt, also
 %parktisch ein Tiefpass anwendet der die hohen Frequenzen unterdrückt
 
-k = 25;  % Größe des Mittelungsfilters (ungerade Zahl!)
+k = 21;  % Größe des Mittelungsfilters (ungerade Zahl!)
 % Box-Filter
-kernel = ones(k) / k^2;
+kernel = ones(k) / k^2; %berechnet arithmetischen Mittelwert in Bereich kxk 
 
-smoothed = conv2(original, kernel, 'same');
-sampled19_AA = interp2(X, Y, smoothed, X19, Y19, 'nearest');
-interp19_AA  = interp2(X19, Y19, sampled19_AA, X, Y, 'linear');
+smoothed = conv2(original, kernel, 'same'); %conv2 ist eine 2D Faltung
+sampled38_AA = interp2(X, Y, smoothed, X38, Y38, 'nearest');
+interp38_AA  = interp2(X38, Y38, sampled38_AA, X, Y, 'linear');
 
 % bringt irgentwie nicht viel
 
@@ -68,10 +66,10 @@ interp19_AA  = interp2(X19, Y19, sampled19_AA, X, Y, 'linear');
 %Bedeutet dass man einfach bewusstvdie maximale Frequenz beschränkt
 %Für 19 ist niquist 9.5 und wwir brauchen frequenz unterhalb dieser
 
-f_sub = 5;
-original_sub = cos(f_sub * R);
-sampled19_sub = interp2(X, Y, original_sub, X19, Y19, 'nearest');
-interp19_sub  = interp2(X19, Y19, sampled19_sub, X, Y, 'linear');
+f_sub = 0.7; % Frequnz Verringerung
+original_sub = cos(f_sub * RR);
+sampled38_sub = interp2(X, Y, original_sub, X38, Y38, 'nearest');
+interp38_sub  = interp2(X38, Y38, sampled38_sub, X, Y, 'linear');
 
 %% ----------
 figure
@@ -79,40 +77,40 @@ figure
 subplot(3,3,1)
 imagesc(original)
 axis image off
-title('Original')
+
 
 subplot(3,3,2)
-imagesc(sampled19)
+imagesc(sampled38)
 axis image off
-title('19×19 gesampelt')
+
 
 subplot(3,3,3)
-imagesc(interp19)
+imagesc(interp38)
 axis image off
-title('19×19 interpoliert')
+
 
 subplot(3,3,5)
 imagesc(sampled13)
 axis image off
-title('13×13 gesampelt')
+
 
 subplot(3,3,6)
 imagesc(interp13)
 axis image off
-title('13×13 interpoliert')
+
 
 subplot(3,3,4)
-imagesc(interp19_AA)
+imagesc(interp38_AA)
 axis image off
-title('19x19 mit Antialiasing')
+
 
 subplot(3,3,7)
 imagesc(smoothed)
 axis image off
-title('gemittelt, also Tiefpass')
+
 colormap gray
 
 subplot(3,3,8)
-imagesc(interp19_sub)
+imagesc(interp38_sub)
 axis image off
-title('19×19 mit Subsampling')
+
